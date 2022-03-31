f6f0671b8ccd41ca2f27645c791dd0472fd281d9
Hikmatulloh <96409206+Hikmatullohprogramist@users.noreply.github.com>
2022-03-31 21:53:14 +0500

    All changes
    Finished Project

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/app_1app/generatedregistrasiyawidget/GeneratedRegistrasiyaWidget.dart';
+import 'package:flutterapp/app_1app/generatedregistrasiyawidget1/GeneratedRegistrasiyaWidget1.dart';
+import 'package:flutterapp/app_1app/generatedregistrasiyawidget2/GeneratedRegistrasiyaWidget2.dart';
+import 'package:flutterapp/app_1app/generatedregistrasiyawidget3/GeneratedRegistrasiyaWidget3.dart';
+import 'package:flutterapp/app_1app/generatedregistrasiyawidget4/GeneratedRegistrasiyaWidget4.dart';
+import 'package:flutterapp/app_1app/generatedregistrasiyawidget5/GeneratedRegistrasiyaWidget5.dart';
+import 'package:flutterapp/app_1app/generatedregistrasiyawidget6/GeneratedRegistrasiyaWidget6.dart';
+import 'package:flutterapp/app_1app/generatedwidget1/GeneratedWidget1.dart';
+import 'package:flutterapp/app_1app/generateddiagnosewidget/GeneratedDiagnoseWidget.dart';
+import 'package:flutterapp/app_1app/generateddiagnosewidget1/GeneratedDiagnoseWidget1.dart';
+import 'package:flutterapp/app_1app/generateddiagnosewidget2/GeneratedDiagnoseWidget2.dart';
+import 'package:flutterapp/app_1app/generateddiagnosewidget3/GeneratedDiagnoseWidget3.dart';
+import 'package:flutterapp/app_1app/generateddiagnosewidget4/GeneratedDiagnoseWidget4.dart';
+import 'package:flutterapp/app_1app/generateddoctorinfowidget/GeneratedDoctorinfoWidget.dart';
+import 'package:flutterapp/app_1app/generateddoctorinfowidget1/GeneratedDoctorinfoWidget1.dart';
+import 'package:flutterapp/app_1app/generateddoctorinfowidget2/GeneratedDoctorinfoWidget2.dart';
+import 'package:flutterapp/app_1app/generatedsearchwidget/GeneratedSearchWidget.dart';
+import 'package:flutterapp/app_1app/generatedschedulewidget/GeneratedScheduleWidget.dart';
+import 'package:flutterapp/app_1app/generateddilshodwidget/GeneratedDilshodWidget.dart';
+import 'package:flutterapp/app_1app/generatedschedulewidget1/GeneratedScheduleWidget1.dart';
+import 'package:flutterapp/app_1app/generatedschedulewidget2/GeneratedScheduleWidget2.dart';
+import 'package:flutterapp/app_1app/generatedschedulewidget3/GeneratedScheduleWidget3.dart';
+import 'package:flutterapp/app_1app/generatedheaderwidget4/GeneratedHeaderWidget4.dart';
+import 'package:flutterapp/app_1app/generatedschedulewidget4/GeneratedScheduleWidget4.dart';
+import 'package:flutterapp/app_1app/generatedheaderwidget5/GeneratedHeaderWidget5.dart';
+import 'package:flutterapp/app_1app/generatedschedulewidget5/GeneratedScheduleWidget5.dart';
+import 'package:flutterapp/app_1app/generatedframe996widget/GeneratedFrame996Widget.dart';
+import 'package:flutterapp/app_1app/generatedframe999widget/GeneratedFrame999Widget.dart';
+import 'package:flutterapp/app_1app/generatedschedulewidget6/GeneratedScheduleWidget6.dart';
+
+void main() {
+  runApp(app_1App());
+}
+
+class app_1App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/GeneratedRegistrasiyaWidget5',
+      routes: {
+        '/GeneratedRegistrasiyaWidget': (context) =>
+            GeneratedRegistrasiyaWidget(),
+        '/GeneratedRegistrasiyaWidget1': (context) =>
+            GeneratedRegistrasiyaWidget1(),
+        '/GeneratedRegistrasiyaWidget2': (context) =>
+            GeneratedRegistrasiyaWidget2(),
+        '/GeneratedRegistrasiyaWidget3': (context) =>
+            GeneratedRegistrasiyaWidget3(),
+        '/GeneratedRegistrasiyaWidget4': (context) =>
+            GeneratedRegistrasiyaWidget4(),
+        '/GeneratedRegistrasiyaWidget5': (context) =>
+            GeneratedRegistrasiyaWidget5(),
+        '/GeneratedRegistrasiyaWidget6': (context) =>
+            GeneratedRegistrasiyaWidget6(),
+        '/GeneratedWidget1': (context) => GeneratedWidget1(),
+        '/GeneratedDiagnoseWidget': (context) => GeneratedDiagnoseWidget(),
+        '/GeneratedDiagnoseWidget1': (context) => GeneratedDiagnoseWidget1(),
+        '/GeneratedDiagnoseWidget2': (context) => GeneratedDiagnoseWidget2(),
+        '/GeneratedDiagnoseWidget3': (context) => GeneratedDiagnoseWidget3(),
+        '/GeneratedDiagnoseWidget4': (context) => GeneratedDiagnoseWidget4(),
+        '/GeneratedDoctorinfoWidget': (context) => GeneratedDoctorinfoWidget(),
+        '/GeneratedDoctorinfoWidget1': (context) =>
+            GeneratedDoctorinfoWidget1(),
+        '/GeneratedDoctorinfoWidget2': (context) =>
+            GeneratedDoctorinfoWidget2(),
+        '/GeneratedSearchWidget': (context) => GeneratedSearchWidget(),
+        '/GeneratedScheduleWidget': (context) => GeneratedScheduleWidget(),
+        '/GeneratedDilshodWidget': (context) => GeneratedDilshodWidget(),
+        '/GeneratedScheduleWidget1': (context) => GeneratedScheduleWidget1(),
+        '/GeneratedScheduleWidget2': (context) => GeneratedScheduleWidget2(),
+        '/GeneratedScheduleWidget3': (context) => GeneratedScheduleWidget3(),
+        '/GeneratedHeaderWidget4': (context) => GeneratedHeaderWidget4(),
+        '/GeneratedScheduleWidget4': (context) => GeneratedScheduleWidget4(),
+        '/GeneratedHeaderWidget5': (context) => GeneratedHeaderWidget5(),
+        '/GeneratedScheduleWidget5': (context) => GeneratedScheduleWidget5(),
+        '/GeneratedFrame996Widget': (context) => GeneratedFrame996Widget(),
+        '/GeneratedFrame999Widget': (context) => GeneratedFrame999Widget(),
+        '/GeneratedScheduleWidget6': (context) => GeneratedScheduleWidget6(),
+      },
+    );
+  }
+}
